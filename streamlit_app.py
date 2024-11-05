90343ee63b381c4693055b1a690c29f23db94715
wrujano <150813496+wrujano@users.noreply.github.com>
2024-11-05 01:21:14 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ from openai import OpenAI
 
 # Título y descripción
 st.title("💬 Chatbot")
-st.write("Este es un chatbot que utiliza diferentes modelos GPT")
+st.write("Este es un chatbot que utiliza diferentes modelos GPT. El modelo que selecciones va a ser el que va a usar durante toda la sesión")
 
 # Lista de modelos disponibles
 model_options = ["gpt-3.5-turbo", "gpt-4-turbo", "gpt-4", "gpt-4o","gpt-4o-mini"]
